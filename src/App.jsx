a83da6dee26141eb1f664f5da8d2e6ac2125ab64
Mukund Kulthe <mukundkulthe12@gmail.com>
2023-11-13 16:26:06 +0530

    I have created Header and TabularWidget component and import header to app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header/header";
 
 function App() {
   return (
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
-  )
+    <>
+      <Header />
+      <h1 className="text-3xl font-bold text-red-500 underline text-center">
+        Hello world!
+      </h1>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>Menu Navigations</div>;
+};
+
+export default Header;
